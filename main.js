5441d61e2699643bfa8f86c82e509c6d96d9a715
Pamodh Yasawardene <31826646+pamodh@users.noreply.github.com>
2018-08-14 01:59:19 +0530

    Replaced with instascan and added initial steps of local storage

--- main.js
+++ main.js
@@ -1,31 +1,88 @@
-$(document).ready(function() {
-    // $('#qr-video').html5_qrcode(function(data){
-    //         alert('Code read: ' + data);
-    //     }, function(error){
-    //         $('#qr-status').html(error);
-    //     }, function(videoError){
-    //         alert('Error opening: ' + videoError);
-    //     });
-    // $('#qr-change-camera').click(function () {
-    //     $('#qr-reader').html5_qrcode_changeCamera();
-    //     $('#qr-status').html('Changed!');
-    // });
+var scanner = null;
+var active_camera = -1;
+var datastore = null;
 
-    let scanner = new Instascan.Scanner({ video: document.getElementById('qr-video') });
-    scanner.addListener('scan', function (content) {
-        $('#qr-status').html('Scanned: ' + content);
-    });
-    Instascan.Camera.getCameras().then(function (cameras) {
-      if (cameras.length > 0) {
-        scanner.start(cameras[0]);
-      } else {
-        alert('No cameras found.');
-      }
-    }).catch(function (e) {
-        $('#qr-status').html('Error: ' + e);
-    });
+$(document).ready(function() {
+    datastore = localforage.createInstance({ name: 'registered-members' });
+    $('#qr-start-btn').click(init_scanner);
+    $('#refresh-data-btn').click(refresh_data);
 });
 
-function code_scanned(code) {
-    
+function init_scanner() {
+    scanner = new Instascan.Scanner({ video: document.getElementById('qr-preview') });
+    scanner.addListener('scan', function (content) {
+        code_scanned(content);
+    });
+    change_camera();
+    $('#qr-change-camera-btn').click(change_camera);
+    $('#qr-start-panel').addClass('hidden');
+    $('#qr-panel').removeClass('hidden');
 }
+
+function change_camera() {
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+            active_camera++;
+            if (active_camera >= cameras.length)
+                active_camera = 0;
+            scanner.start(cameras[active_camera]);
+        } else {
+            handle_error('No cameras found on your device! Check permissions!');
+        }
+    }).catch(function (e) {
+        handle_error('Error accessing camera! Check permissions!', e);
+    });
+}
+
+function code_scanned(code) {
+    let code = String(code);
+    datastore.getItem(code).then(function (record) {
+        if (record == null) {
+            if (window.confirm('This person has not registered! Scan anyway?')) {
+                record = {
+                    id: code,
+                    scantime: new Date(),
+                    walk_in: true
+                };
+                datastore.setItem(code, record).then().catch(function (e) {
+                    handle_error('Error storing data locally', e)
+                });
+            }
+        } else if (record.scantime != null) {
+            window.alert('Already scanned at ' + record.scantime);
+        } else {
+            if (window.confirm('Scan ' + code + '?')) {
+                record.scantime = new Date();
+                datastore.setItem(code, record).then().catch(function (e) {
+                    handle_error('Error storing data locally', e)
+                });
+            }
+        }
+    }).catch(function (e) {
+        handle_error('Error accessing locally stored data', e);
+    })
+}
+
+function refresh_data() {
+    $('#data-table').find('tbody').empty();
+    $('#refresh-data-btn').addClass('hidden');
+    localforage.iterate(function (id, record, i) {
+        $('#data-table').append(String.format(
+            '<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td>',
+            record.id,
+            record.name,
+            record.walk_in ? 'Walk-in' : record.register_time,
+            record.scantime
+        ));
+    }).then(function() {
+        $('#refresh-data-btn').removeClass('hidden');
+    }).catch(function (e) {
+        $('#refresh-data-btn').removeClass('hidden');
+        handle_error('Could not read data from local storage', e);
+    });
+}
+
+function handle_error(message, e='') {
+    alert(message + ' ' + e);
+}
+
